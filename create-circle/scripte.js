de5d0ad05d7b6577e812314695b13c54164f5bbd
Lanita13 <makharadzelana13@gmail.com>
2025-02-18 15:16:22 +0400

    update

---
+++ create-circle/scripte.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const circles = document.querySelectorAll(".circle");
+    
+//     setInterval(() => {
+//         circles.forEach(circle => {
+//             let randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+//             circle.style.borderColor = randomColor;
+//             circle.style.boxShadow = `0 0 15px ${randomColor}`;
+//         });
+//     }, 1000);
+// });
